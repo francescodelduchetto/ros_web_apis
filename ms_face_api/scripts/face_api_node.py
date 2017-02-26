4816ea0c8af43cd97469ed4b68b17e668f70588a
Marc Hanheide <marc@hanheide.net>
2017-02-26 21:55:07 +0000

    first time working with ROS

--- ms_face_api/scripts/face_api_node.py
+++ ms_face_api/scripts/face_api_node.py
@@ -1,8 +1,29 @@
 import cognitive_face as CF
+import rospy
+from cv_bridge import CvBridge
+from cv2 import startWindowThread, imencode
+from StringIO import StringIO
+from sensor_msgs.msg import Image
 
-KEY = 'be062e88698e4777ac6196623d7230dd'  # Replace with a valid Subscription Key here.
-CF.Key.set(KEY)
+class CognitiveFaceROS:
 
-img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
-result = CF.face.detect(img_url, landmarks=True, attributes='age,gender,headPose,smile,facialHair,glasses')
-print result
+    def __init__(self):
+        self._api_key = rospy.get_param('~key',
+                                        'be062e88698e4777ac6196623d7230dd')
+        self._cv_bridge = CvBridge()
+        startWindowThread()
+        CF.Key.set(self._api_key)
+
+    def detect(self, image):
+        img = self._cv_bridge.imgmsg_to_cv2(image, "bgr8")
+        retval, buf = imencode('.jpg', img)
+        strio = StringIO(buf.tostring())
+        return CF.face.detect(strio,
+                              landmarks=True,
+                              attributes='age,gender,headPose,smile,glasses')
+
+
+rospy.init_node('cogntivefaceapi')
+cfa = CognitiveFaceROS()
+msg = rospy.wait_for_message('/image', Image)
+print cfa.detect(msg)
