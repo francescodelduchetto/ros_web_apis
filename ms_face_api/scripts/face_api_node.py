456552e4de00a0fa51c6da859985a40369b73841
Marc Hanheide <marc@hanheide.net>
2017-02-26 22:18:15 +0000

    added service

--- ms_face_api/scripts/face_api_node.py
+++ ms_face_api/scripts/face_api_node.py
@@ -4,6 +4,8 @@ from cv_bridge import CvBridge
 from cv2 import startWindowThread, imencode
 from StringIO import StringIO
 from sensor_msgs.msg import Image
+from ms_face_api.srv import PersonGroupCreate
+
 
 class CognitiveFaceROS:
 
@@ -13,6 +15,13 @@ class CognitiveFaceROS:
         self._cv_bridge = CvBridge()
         startWindowThread()
         CF.Key.set(self._api_key)
+        self._srv_person_group_create = rospy.Service('person_group_create',
+                                                      PersonGroupCreate,
+                                                      self.person_group_create)
+
+    def person_group_create(self, req):
+        print req
+        return []
 
     def detect(self, image):
         img = self._cv_bridge.imgmsg_to_cv2(image, "bgr8")
@@ -25,5 +34,6 @@ class CognitiveFaceROS:
 
 rospy.init_node('cogntivefaceapi')
 cfa = CognitiveFaceROS()
-msg = rospy.wait_for_message('/image', Image)
-print cfa.detect(msg)
+#msg = rospy.wait_for_message('/image', Image)
+#print cfa.detect(msg)
+rospy.spin()
