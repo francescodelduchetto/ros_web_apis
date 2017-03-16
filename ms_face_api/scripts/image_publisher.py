11a2e477e23119afec3508053469972fecfbcc8f
Marc Hanheide <marc@hanheide.net>
2017-03-16 16:31:21 +0000

    support a rate to republish image

--- ms_face_api/scripts/image_publisher.py
+++ ms_face_api/scripts/image_publisher.py
@@ -11,14 +11,23 @@ class ImageLatchedPublisher:
     def __init__(self):
         self._cv_bridge = CvBridge()
         self._filename = rospy.get_param('~file', 'image.jpg')
+        self._rate = rospy.get_param('~rate', -1)
         self._pub = rospy.Publisher('image', Image,
-                                    latch=True, queue_size=1)
+                                    latch=True, queue_size=10)
 
     def spin(self):
         img = imread(self._filename)
         msg = self._cv_bridge.cv2_to_imgmsg(img, 'bgr8')
-        self._pub.publish(msg)
-        rospy.spin()
+
+        if self._rate > 0:
+            r = rospy.Rate(self._rate)
+
+            while not rospy.is_shutdown():
+                self._pub.publish(msg)
+                r.sleep()
+        else:
+            self._pub.publish(msg)
+            rospy.spin()
 
 
 rospy.init_node('image_latch_publisher', anonymous=True)
